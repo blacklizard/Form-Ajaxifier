7bc54d173d213380c95fffe6ff80f25b791fcc75
Jeevan@blacklizard <jeevan@pocketpixel.com>
2015-08-04 00:56:45 +0800

    Enable listening to newly added from to DOM

--- ajaxifier.js
+++ ajaxifier.js
@@ -15,34 +15,30 @@
             global: false
         }, options);
 
-        return this.each(function () {
-
+        return $(document).on('submit', 'form', function (event) {
+            var self = $(this);
             if (!$(this).hasClass('no-ajax')) {
-                return $(this).submit(function (event) {
-                    var self = $(this);
-                    $.ajax({
-                        method: self.attr('method'),
-                        url: self.attr('action'),
-                        data: self.serialize(),
-                        global: settings.global,
-                        beforeSend: function () {
-                            settings.beforeSend(self);
-                        },
-                        success: function (data, textStatus, jqXHR) {
-                            settings.success(self, data, textStatus, jqXHR);
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            settings.error(self, jqXHR, textStatus, errorThrown);
-                        },
-                        complete: function (jqXHR, textStatus) {
-                            settings.complete(self, jqXHR, textStatus);
-                        }
-                    });
-                    event.preventDefault();
-                    event.stopImmediatePropagation();
+                $.ajax({
+                    method: self.attr('method'),
+                    url: self.attr('action'),
+                    data: self.serialize(),
+                    global: settings.global,
+                    beforeSend: function () {
+                        settings.beforeSend(self);
+                    },
+                    success: function (data, textStatus, jqXHR) {
+                        settings.success(self, data, textStatus, jqXHR);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        settings.error(self, jqXHR, textStatus, errorThrown);
+                    },
+                    complete: function (jqXHR, textStatus) {
+                        settings.complete(self, jqXHR, textStatus);
+                    }
                 });
+                event.preventDefault();
+                event.stopImmediatePropagation();
             }
         });
     }
-
 }(jQuery));
